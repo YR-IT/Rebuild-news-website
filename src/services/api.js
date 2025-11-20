b5151d369cbc046bdd5bd8be5603e487d7ab50cf
Ashish <90093173+ashurohilla@users.noreply.github.com>
2025-11-20 20:55:51 +0530

    AD

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://rebuilt-backend-beta.vercel.app"|| 'http://localhost:4000'
+const API_BASE_URL = "https://rebuilt-backend-beta.vercel.app";
 
 function getAuthHeaders() {
 	const token = localStorage.getItem('token')
